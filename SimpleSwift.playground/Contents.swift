fbbcbfe071711b38a9d4b7d8c23184d9ce4f81d3
divij <divij@uw.edu>
2021-04-16 23:16:55 -0700

    simple calc upload

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,11 +1,76 @@
+import Foundation
+
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    var index = 0
+    
+    if args[args.count - 1].elementsEqual("count") {
+        var counter = 0
+        for elem in args {
+            if Int(elem) != nil {
+                counter += 1
+            }
+        }
+        return counter
+    } else if args[args.count - 1].elementsEqual("avg") {
+        if args.count == 1 {
+            return 0
+        }
+        var sum = 0
+        for elem in args {
+            let num = Int(elem)
+            if num != nil {
+                sum += num!
+            }
+        }
+        return sum / (args.count - 1)
+    } else if args[args.count - 1].elementsEqual("fact") {
+        if args.count == 1 {
+            return 0
+        }
+        return Int(factorial(factorialNumber: UInt64(args[0])!))
+    } else {
+        var e1 = 0
+        var e2 = 1
+        var oper = ""
+        
+        for elem in args {
+            if index == 0 {
+                e1 = Int(elem)!
+            } else if index == 1 {
+                oper = elem
+            } else  {
+                e2 = Int(elem)!
+            }
+            index += 1
+        }
+        if oper.elementsEqual("+") {
+            return e1 + e2
+        } else if oper.elementsEqual("-") {
+            return e1 - e2
+        } else if oper.elementsEqual("/") {
+            return e1 / e2
+        } else if oper.elementsEqual("%") {
+            return e1 % e2
+        } else {
+            return e1 * e2
+        }
+    }
+   // return -1
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    let arr = arg.components(separatedBy: " ")
+    return calculate(arr)
+}
+
+func factorial(factorialNumber: UInt64) -> UInt64 {
+    if factorialNumber == 0 {
+        return 1
+    } else {
+        return factorialNumber * factorial(factorialNumber: factorialNumber - 1)
+    }
 }
 
 // -------------------------------------------
